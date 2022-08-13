21082cb3ddff59147d95f04b20450e50e227cc28
Arjun <vaddadiarjun2003@gmail.com>
2022-08-13 12:04:12 +0530

    add cursor as argument to select and add events

--- db.py
+++ db.py
@@ -14,26 +14,29 @@ def create_table():
 
 # add _event function basically adds, information to the reminders table
 
-def add_event(name, day, month):
-    cur.execute("INSERT INTO reminders VALUES('{}',{},{});".format(name, day, month))
+def add_event(cur, event_name, day, month):
+    cur.execute("INSERT INTO reminders VALUES('{}',{},{});".format(event_name, day, month))
     print("EVENT ADDED SUCCESSFUL!")
     con.commit()
 
  # select_event function bascially selects the event with current_date.   
 
-def select_events(within_days):
+def select_events(cur, within_days):
     query=("SELECT DISTINCT name,day,month FROM reminders WHERE day>extract(day from CURRENT_DATE) AND day<extract(day from CURRENT_DATE)+{} AND extract(month from CURRENT_DATE)=month;".format(within_days))
     cur.execute(query)
     events = cur.fetchall()
     con.commit()
-    return events   
+    return events
+
 
 if __name__ == "__main__" :
     con = connect_todatabase('reminders')    
     cur = getcursor(con)
-    r = add_event('r birthday', 10, 8)
-    events = select_events(2)
+    r = add_event(cur, 'r_birtday',11,8)
+    events = select_events(cur, 2)
     print(events)
+
+
 
 
 
